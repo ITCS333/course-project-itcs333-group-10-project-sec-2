6988e88a4e1377eb5406c614b44aefe7b9e3592a
MasterMohsen <mohsengamer1141@gmail.com>
2025-12-18 19:31:39 +0300

    Update index.php

--- src/assignments/api/index.php
+++ src/assignments/api/index.php
@@ -72,7 +72,7 @@ $db = $database->getConnection();
 
 
 try {
-    $db = new PDO('mysql:host=localhost;dbname=your_database_name;charset=utf8mb4', 'your_username', 'your_password');
+   
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
